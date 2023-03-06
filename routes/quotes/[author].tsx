6b88b2751a68837100cbc18aa1e611029d54af6e
Derik van Schaik <derikvanschaik@gmail.com>
2023-03-05 23:08:00 -0800

    add quote grid list to quotes by author page

--- routes/quotes/[author].tsx
+++ routes/quotes/[author].tsx
@@ -1,12 +1,12 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import NavBar from '../../components/NavBar.tsx';
-import QuoteCard from '../../components/QuoteCard.tsx';
+import QuoteList from '../../components/QuoteList.tsx';
 
 export const handler: Handlers = {
   async GET(_, ctx) {
     const authorName = ctx.params.author;
     const resp = await fetch(`${Deno.env.get("API_URL")}/quotes/${authorName}`);
-    const quotes = ( await resp.json() ).map( data => data.quote );
+    const quotes = await resp.json();
     return ctx.render({ quotes , author: authorName })
     
   },
@@ -18,9 +18,7 @@ export default function Greet({params, data }: PageProps) {
     <div class='w-3/4 mx-auto my-0' id="top">
       <h1 
         class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Quotes by <span class="text-blue-600 dark:text-blue-500">{data.author}</span> :</h1>
-      <ul>
-        { data.quotes.map(quote => <QuoteCard quote={quote} />)}
-      </ul>
+      <QuoteList quotes={data.quotes} />
     </div>
     </div>
   )
